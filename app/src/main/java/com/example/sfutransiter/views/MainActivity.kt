b0302565ec1da40fa93b8a8ee0e28f4a9567bdb6
johnnyd <johnnyd@sfu.ca>
2022-11-21 16:25:24 -0800

    Add view binding, convert to fragments

--- app/src/main/java/com/example/sfutransiter/views/MainActivity.kt
+++ app/src/main/java/com/example/sfutransiter/views/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.sfutransiter.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.sfutransiter.R
+import com.example.sfutransiter.databinding.ActivityMainBinding
+import com.example.sfutransiter.views.components.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/sfutransiter/views/search_by/SearchBy.kt
+++ app/src/main/java/com/example/sfutransiter/views/search_by/SearchBy.kt
@@ -1,12 +1,27 @@
 package com.example.sfutransiter.views.search_by
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.sfutransiter.R
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sfutransiter.databinding.FragmentSearchByBinding
 
-class SearchBy : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_by)
+class SearchBy : Fragment() {
+    private var _binding: FragmentSearchByBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchByBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = SearchBy()
     }
 }
