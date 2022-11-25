8b20eb0af845e95f7727e8347904196ebe24db8a
johnnyd <johnnyd@sfu.ca>
2022-11-25 12:37:34 -0800

    Create ping endpoint to AWS, organize and abstract

--- app/src/main/java/com/example/sfutransiter/backend/RetrofitAPI.kt
+++ app/src/main/java/com/example/sfutransiter/backend/RetrofitAPI.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 private const val BASE_TRANSLINK_API_URL = "https://api.translink.ca/rttiapi/v1/"
-private const val BASE_AWS_API_URL = "https://xvgz7q0bol.execute-api.us-west-2.amazonaws.com/V1"
+private const val BASE_AWS_API_URL = "https://xvgz7q0bol.execute-api.us-west-2.amazonaws.com/V1/"
 
 object RetrofitAPI {
     fun getTransLinkInstance(): Retrofit {
